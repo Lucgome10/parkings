f1427036ad56b34778646d0883d425603f61bc0c
lukegom94 <83473675+lukegom94@users.noreply.github.com>
2022-05-08 11:54:19 +0200

    deleted project

--- src/main/kotlin/sanviator/tfg/parkings/persistence/repositories/ParkingLotsRepository.kt
+++
@@ -1,7 +0,0 @@
-package sanviator.tfg.parkings.persistence.repositories
-
-import org.springframework.data.jpa.repository.JpaRepository
-import sanviator.tfg.parkings.persistence.dao.ParkingLotDAO
-
-internal interface ParkingLotsRepository : JpaRepository<ParkingLotDAO, String>{
-}
